b3a9f685bafb644b9d23d7ed6e9aaa8e47b4874a
aymene01 <aymene01@hotmail.fr>
2024-02-29 11:30:29 +0100

    prettier

--- packages/toolbox/src/env/getListenAddr.ts
+++ packages/toolbox/src/env/getListenAddr.ts
@@ -1,16 +1,14 @@
-import { getString } from "./getString";
+import { getString } from './getString'
 
 export function getListenAddr(key: string, defaultValue: string) {
-  const value = getString(key, defaultValue);
-  const [host, portStr] = value?.split(":") as string[];
+  const value = getString(key, defaultValue)
+  const [host, portStr] = value?.split(':') as string[]
 
-  const port = parseInt(portStr);
+  const port = parseInt(portStr)
 
   if (isNaN(port)) {
-    throw new Error(
-      `Cannot parseInt(${portStr}) for environment variable "${key}"`,
-    );
+    throw new Error(`Cannot parseInt(${portStr}) for environment variable "${key}"`)
   }
 
-  return { host, port };
+  return { host, port }
 }
