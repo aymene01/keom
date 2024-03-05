b27d646e3b52558dbefb9bc0b79f1e6970a54546
aymene01 <aymene01@hotmail.fr>
2024-03-05 13:44:55 +0100

    refacto folders

--- apps/web/src/app/(application)/page.tsx
+++ apps/web/src/app/(application)/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import { Button } from '@keom/react-ui'
-import { signOut } from '@keom/auth/react'
+import { signOut, useSession } from '@keom/auth/react'
 
 export default function Home() {
+  const { data } = useSession()
+
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col space-y-4">
       <h1>Hey from Keom App</h1>
-      <Button onClick={() => signOut()}>Button</Button>
+      <p className="text-gray-500 text-sm">{data?.user?.email}</p>
+      <Button onClick={() => signOut()}>Logout</Button>
     </div>
   )
 }
