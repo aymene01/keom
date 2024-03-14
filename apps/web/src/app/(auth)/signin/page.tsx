fed09e6ad78c2906ded1dcb07e181c60e77d9560
aymene01 <aymene01@hotmail.fr>
2024-03-14 18:13:20 +0100

    fix(web): signin page text

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -8,8 +8,8 @@ export default function SignIn() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-50 to-blue-100">
       <div className="p-6 rounded-lg bg-white shadow-lg flex flex-col space-y-4 text-center">
-        <h1 className="text-2xl text-gray-800 font-light">Welcome to Keom Platform</h1>
-        <p className="text-gray-500">Streamline your workflow with a single click</p>
+        <h1 className="text-2xl text-gray-800 font-light">Welcome to Keom Application</h1>
+        <p className="text-gray-500">Create your group and start making impact arround you</p>
         <div className="pt-4">
           <div className="inline-block">
             <Button size="lg" onClick={() => signIn('google')} leftIcon={<FcGoogle />}>
